abd74b6bfa241daaec63409b273f47ee1766ae87
Nereu Vítor <nereuvitor12@gmail.com>
2026-04-03 14:16:50 -0300

    chore: initial spring boot project structure

---
+++ localeats-api/src/test/java/com/nereuvitor/localeats_api/LocaleatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nereuvitor.localeats_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaleatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
