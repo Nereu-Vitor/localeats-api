d4877a18d16e24677935a62108f5381e1e0aa1ec
Nereu Vítor <nereuvitor12@gmail.com>
2026-04-04 13:51:08 -0300

    feat: implement Product entity

---
+++ localeats-api/src/main/java/com/nereuvitor/localeatsapi/model/Product.java
@@ -0,0 +1,47 @@
+package com.nereuvitor.localeatsapi.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = Product.TABLE_NAME)
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Product implements Serializable {
+
+    public static final String TABLE_NAME = "product";
+
+    private static final long serialVersionUID = 1L; 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+}
