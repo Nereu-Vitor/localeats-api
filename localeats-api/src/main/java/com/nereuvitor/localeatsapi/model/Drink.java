a4aecebab7367b9688ff0c26b2dcd19a307ae1b0
Nereu Vítor <nereuvitor12@gmail.com>
2026-04-05 20:35:28 -0300

    feat: implement Drink entity

---
+++ localeats-api/src/main/java/com/nereuvitor/localeatsapi/model/Drink.java
@@ -0,0 +1,32 @@
+package com.nereuvitor.localeatsapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = Drink.TABLE_NAME)
+@PrimaryKeyJoinColumn(name = "id")
+public class Drink extends Product {
+
+    public static final String TABLE_NAME = "drink";
+    
+    @NotNull(message = "O volume em ml é obrigatório")
+    private Integer volumeMl;
+
+    @NotBlank(message = "O tipo da bebida é obrigatório")
+    private String drinkType;
+
+}
