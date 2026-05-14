2f77dd375f84ad38ea9468503b122299b024c55b
Nereu Vítor <nereuvitor12@gmail.com>
2026-05-14 15:39:05 -0300

    feat(security): update Securityconfig with UserDetailsService and AuthenticationManager

--- localeats-api/src/main/java/com/nereuvitor/localeatsapi/configs/SecurityConfig.java
+++ localeats-api/src/main/java/com/nereuvitor/localeatsapi/configs/SecurityConfig.java
@@ -3,18 +3,35 @@ package com.nereuvitor.localeatsapi.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nereuvitor.localeatsapi.security.JWTUtil;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     private static final String[] PUBLIC_MATCHERS = {
             "/users",
